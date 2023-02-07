0f58c641da726b21feadac60d3f834e29757ab55
dltprjs1 <107850856+dltprjs1@users.noreply.github.com>
2023-02-07 13:51:59 +0900

    Add files via upload

---
+++ hello-spring/src/main/java/hello/hellospring/AOP/TimeTraceAOP.java
@@ -0,0 +1,24 @@
+package hello.hellospring.AOP;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect //AOP는 Aspect 에노테이션이 필요하다.
+@Component
+public class TimeTraceAOP {
+    @Around("execution(* hello.hellospring..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable{
+        long start = System.currentTimeMillis();
+        System.out.println("START : "+joinPoint.toString());
+        try{
+            Object result = joinPoint.proceed();
+            return result;
+        }finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END : "+joinPoint.toString()+" "+timeMs+"ms");
+        }
+    }
+}
